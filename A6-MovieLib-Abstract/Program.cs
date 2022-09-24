ecd9c0592f5f4b67847bc0e1704b574c11029ab6
cmoldenauer <97994521+cmoldenauer@users.noreply.github.com>
2022-09-24 14:41:16 -0500

    Add project files.

---
+++ A6-MovieLib-Abstract/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace A6_MovieLib_Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
